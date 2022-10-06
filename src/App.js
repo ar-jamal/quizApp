a96f92991fa16e91cf2473994b90228a06d9efb9
ar-jamal <ar.jamalkarim@gmail.com>
2022-10-06 20:46:50 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Data, { Quizes } from './utils/Data';
+import { useEffect, useState } from 'react';
+import Checkbox from '@mui/material/Checkbox';
 
 function App() {
+  const [data, setData] = useState(Data);
+  const [ind, setInd] = useState(0);
+  const arrLength = data[ind].Opt.length;
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+  const checkAns = (val) => {
+    const isRight = data.filter((e) => e.Opt.length === val.length)
+  }
+
+  const options = () => {
+    for (let i = 0; i < arrLength; i++) {
+      return (
+        <div>
+          <Checkbox {...label} checked={false} />
+          <div
+            key={ind}
+            onClick={(e) => checkAns(e.target.value)}
+          >{data[ind].Opt[i]}
+          </div>
+        </div>
+      )
+    }
+  };
+  useEffect(() => {
+    options();
+  }, [ind])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Quiz App</h1>
       </header>
+      {/* console.log(quizes) */}
+      <div className='Body'>
+        <p style={{ color: "white" }}>{data["Q"]}</p>
+        {options()};
+      </div>
     </div>
   );
 }
---
+++ src/utils/Data.js
@@ -0,0 +1,105 @@
+
+const Data = [
+    {
+        Q: "Q-01: Which data structure uses LIFO?",
+        Opt: [
+            "A: Array",
+            "B: Int",
+            "C: Stacks                          ",
+            "D: Qeues"
+        ]
+    },
+    {
+        Q: "Q-02: What is FIFO ??",
+        Opt: [
+            "A: First in Few Out",
+            "B: Few In Few out",
+            "C: First In First Out",
+            "D: Few In First Out"
+        ]
+    },
+    {
+        Q: "Q-03: Int hold decemals numbers",
+        Opt: [
+            "A: True",
+            "B: False",
+        ]
+    },
+    {
+        Q: "Q-04: One loop inside the body of another loop is called",
+        Opt: [
+            "A: Loop in loop",
+            "B: Double loops",
+            "C: Nested",
+        ]
+    },
+    {
+        Q: "Q-05: What does this eqation mean ? a != t",
+        Opt: [
+            "A: A is assigned t",
+            "B: A and t are equl",
+            "C: A is not eqal to t",
+            "D: T is add to a",
+        ]
+    },
+    {
+        Q: "Q-06: What commmand do you use to output data to the screen",
+        Opt: [
+            "A: Cin <<",
+            "B: Cout >>",
+            "C: Cout <<",
+            "D: Output >>",
+        ]
+    },
+    {
+        Q: "Q-07: Entering comments is a useless task, it will not help in any way, State true or false",
+        Opt: [
+            "A: True",
+            "B: False",
+        ]
+    },
+    {
+        Q: "Q-08: A Syntax Error is ?",
+        Opt: [
+            "A: An error you will never find",
+            "B: An error you find at the end when the program gives out a wrong value due to logic error",
+            "C: An error caused by language rules being broken",
+            "D: An error due to user error",
+        ]
+    },
+    {
+        Q: "Q-09: A memory location that holds a single letter or number is called _________",
+        Opt: [
+            "A: Double",
+            "B: Int",
+            "C: Char",
+            "D: Word",
+        ]
+    },
+    {
+        Q: "Q10: A short section of code written to complete a task.",
+        Opt: [
+            "A: Buffer",
+            "B: Array",
+            "C: Function",
+            "D: Loop",
+        ]
+    },
+    {
+        Q: "Q11: A do while and a while loop are the same. State true or false",
+        Opt: [
+            "A: True",
+            "B: False",
+        ]
+    },
+    {
+        Q: "Q12: What is the only thing a computer can understand",
+        Opt: [
+            "A: Machine Code",
+            "B: Low level languages",
+            "C: High level languages",
+            "D: Algorithms"
+        ]
+    },
+]
+export default Data;
