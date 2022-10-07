a79cfe147dfaea78627fe9c5a427891ef73029b8
ar-jamal <ar.jamalkarim@gmail.com>
2022-10-07 18:28:33 +0500

    quiz app done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,84 @@
 import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Data, { Quizes } from './utils/Data';
+import Data from './utils/Data';
 import { useEffect, useState } from 'react';
 import Checkbox from '@mui/material/Checkbox';
+import { Button } from '@mui/material';
 
 function App() {
-  const [data, setData] = useState(Data);
-  const [ind, setInd] = useState(0);
-  const arrLength = data[ind].Opt.length;
   const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
-  const checkAns = (val) => {
-    const isRight = data.filter((e) => e.Opt.length === val.length)
+  const [data, setData] = useState(Data);
+  const [ind, setInd] = useState(0);
+  const [optInd, setOptInd] = useState("");
+  const last = ind === data.length - 1
+  const submited = ind === data.length;
+  const question = !submited ? data[ind] : { Opt: [], Q: 'Quiz Submitted' }
+  const score = submited ? data.reduce((acc, que) => acc += que.isRight ? 1 : 0, 0) : 0
+
+  const checkAns = (option) => {
+    const isRight = question.correct === option;
+    let copyData = [...data]
+    console.log(question)
+    copyData[ind] = { ...question, selected: option, isRight }
+    setData(copyData)
   }
 
-  const options = () => {
-    for (let i = 0; i < arrLength; i++) {
-      return (
-        <div>
-          <Checkbox {...label} checked={false} />
-          <div
-            key={ind}
-            onClick={(e) => checkAns(e.target.value)}
-          >{data[ind].Opt[i]}
-          </div>
-        </div>
-      )
-    }
+  function options() {
+
+    const options = question.Opt ?? {}
+    const selected = question.selected
+    return Object.keys(options).map((option, i) => {
+      return (<div className='Quiz-div'
+        key={i}
+        onClick={checkAns.bind(this, option)}
+      >
+        <Checkbox {...label} checked={selected === option} />
+        <div className='Options' >{options[option]}</div>
+      </div>)
+    })
   };
+
   useEffect(() => {
     options();
   }, [ind])
 
   return (
+
     <div className="App">
-      <header className="App-header">
-        <h1>Quiz App</h1>
-      </header>
-      {/* console.log(quizes) */}
-      <div className='Body'>
-        <p style={{ color: "white" }}>{data["Q"]}</p>
-        {options()};
+      <div className='content'>
+        <header className="App-header">
+          <h2> Quiz App </h2>
+        </header>
+        {/* console.log(quizes) */}
+        <div className='Body'>
+          <p className='title'>{question.Q}</p>
+          {
+            !submited &&
+            <div className='Quiz-div-main'>{options()}</div>
+          }
+          <div className='Score-div'>
+            {
+              submited &&
+              <h1 style={{ color: 'black' }}>{"Your Score is:" + score}</h1>
+            }
+
+          </div>
+          {
+            !submited &&
+            <div className='btn-row'>
+              {
+                ind !== 0 ?
+                  <button className='app-btn' onClick={() => setInd(i => i - 1)}>Previous |</button> :
+                  <div className='app-btn' />
+              }
+              <h1 style={{ color: 'white' }}></h1>
+              <button className='app-btn' onClick={() => setInd(i => submited ? i : i + 1)}>| {last ? "Submit" : "Next"}</button>
+            </div>
+          }
+
+        </div>
       </div>
     </div>
   );
--- src/utils/Data.js
+++ src/utils/Data.js
@@ -2,104 +2,116 @@
 const Data = [
     {
         Q: "Q-01: Which data structure uses LIFO?",
-        Opt: [
-            "A: Array",
-            "B: Int",
-            "C: Stacks                          ",
-            "D: Qeues"
-        ]
+        Opt: {
+            A : "Array",
+            B: "Int",
+            C: "Stacks",      
+            D: "Qeues"
+        },
+        correct: "C"
     },
     {
         Q: "Q-02: What is FIFO ??",
-        Opt: [
-            "A: First in Few Out",
-            "B: Few In Few out",
-            "C: First In First Out",
-            "D: Few In First Out"
-        ]
+        Opt: {
+            A: "First in Few Out",
+            B: "Few In Few out",
+            C: "First In First Out",
+            D: "Few In First Out"
+        },
+        correct: "A"
     },
     {
         Q: "Q-03: Int hold decemals numbers",
-        Opt: [
-            "A: True",
-            "B: False",
-        ]
+        Opt: {
+            A: "True",
+            B: "False",
+        },
+        correct: "B"
     },
     {
         Q: "Q-04: One loop inside the body of another loop is called",
-        Opt: [
-            "A: Loop in loop",
-            "B: Double loops",
-            "C: Nested",
-        ]
+        Opt: {
+            A: "Loop in loop",
+            B: "Double loops",
+            C: "Nested",
+        },
+        correct: "C"
     },
     {
         Q: "Q-05: What does this eqation mean ? a != t",
-        Opt: [
-            "A: A is assigned t",
-            "B: A and t are equl",
-            "C: A is not eqal to t",
-            "D: T is add to a",
-        ]
+        Opt: {
+            A: "A is assigned t",
+            B: "A and t are equl",
+            C: "A is not eqal to t",
+            D: "T is add to a",
+        },
+        correct: "B"
     },
     {
         Q: "Q-06: What commmand do you use to output data to the screen",
-        Opt: [
-            "A: Cin <<",
-            "B: Cout >>",
-            "C: Cout <<",
-            "D: Output >>",
-        ]
+        Opt: {
+            A: "Cin <<",
+            B: "Cout >>",
+            C: "Cout <<",
+            D: "Output >>",
+        },
+        correct: "A"
     },
     {
         Q: "Q-07: Entering comments is a useless task, it will not help in any way, State true or false",
-        Opt: [
-            "A: True",
-            "B: False",
-        ]
+        Opt: {
+            A: "True",
+            B: "False",
+        },
+        correct: "A"
     },
     {
         Q: "Q-08: A Syntax Error is ?",
-        Opt: [
-            "A: An error you will never find",
-            "B: An error you find at the end when the program gives out a wrong value due to logic error",
-            "C: An error caused by language rules being broken",
-            "D: An error due to user error",
-        ]
+        Opt: {
+            A: "An error you will never find",
+            B: "An error you find at the end when the program gives out a wrong value due to logic error",
+            C: "An error caused by language rules being broken",
+            D: "An error due to user error",
+        },
+        correct: "C"
     },
     {
         Q: "Q-09: A memory location that holds a single letter or number is called _________",
-        Opt: [
-            "A: Double",
-            "B: Int",
-            "C: Char",
-            "D: Word",
-        ]
+        Opt: {
+            A: "Double",
+            B: "Int",
+            C: "Char",
+            D: "Word",
+        },
+        correct: "B"
     },
     {
         Q: "Q10: A short section of code written to complete a task.",
-        Opt: [
-            "A: Buffer",
-            "B: Array",
-            "C: Function",
-            "D: Loop",
-        ]
+        Opt: {
+            A: "Buffer",
+            B: "Array",
+            C: "Function",
+            D: "Loop",
+        },
+        correct: "D"
     },
     {
         Q: "Q11: A do while and a while loop are the same. State true or false",
-        Opt: [
-            "A: True",
-            "B: False",
-        ]
+        Opt: {
+            A: "True",
+            B: "False",
+        },
+        correct: "A"
     },
     {
         Q: "Q12: What is the only thing a computer can understand",
-        Opt: [
-            "A: Machine Code",
-            "B: Low level languages",
-            "C: High level languages",
-            "D: Algorithms"
-        ]
+        Opt: {
+            A: "Machine Code",
+            B: "Low level languages",
+            C: "High level languages",
+            D: "Algorithms"
+        },
+        correct: "B"
     },
 ]
 export default Data;
